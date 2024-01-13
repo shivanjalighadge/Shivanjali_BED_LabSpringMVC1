b0cfd1ec1c572de45c7cb340fca95127d4e5061e
shivanjalighadge <142910277+shivanjalighadge@users.noreply.github.com>
2024-01-13 20:11:09 +0530

    EmployeeService.java

--- EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/service/EmployeeService.java
+++ EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/service/EmployeeService.java
@@ -1,5 +1,17 @@
 package com.gl.springboot.EmployeeManagement.service;
 
-public interface EmployeeService {
+import java.util.List;
 
+import com.gl.springboot.EmployeeManagement.entity.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	
+	Employee saveEmployee(Employee employee);
+	
+	Employee getEmployeeById(Long id);
+	
+	Employee updateEmployee(Employee employee);
+	
+	void deleteEmployeeById(Long id);
 }
