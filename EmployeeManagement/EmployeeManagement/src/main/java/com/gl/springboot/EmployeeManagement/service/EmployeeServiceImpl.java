299852dc79a19a30ff7cd8881a05f87322b42839
shivanjalighadge <142910277+shivanjalighadge@users.noreply.github.com>
2024-01-13 20:12:06 +0530

    EmployeeServiceImpl.java

--- EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/service/EmployeeServiceImpl.java
+++ EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/service/EmployeeServiceImpl.java
@@ -1,5 +1,41 @@
-package com.gl.springboot.EmployeeManagement.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class EmployeeServiceImpl {
+import com.gl.springboot.EmployeeManagement.entity.Employee;
+import com.gl.springboot.EmployeeManagement.repo.EmployeeRepository;
 
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee getEmployeeById(Long id) {
+		Optional<Employee> employeeOptional= employeeRepository.findById(id);
+		return employeeOptional.get();
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public void deleteEmployeeById(Long id) {
+		employeeRepository.deleteById(id);
+		
+	}
+
+	
 }
