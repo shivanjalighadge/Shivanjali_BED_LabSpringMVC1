e7212d04c2f31618695095386942810bbc295283
shivanjalighadge <142910277+shivanjalighadge@users.noreply.github.com>
2024-01-15 16:34:26 +0530

    EmployeeRepository.java

--- EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/repo/EmployeeRepository.java
+++ EmployeeManagement/EmployeeManagement/src/main/java/com/gl/springboot/EmployeeManagement/repo/EmployeeRepository.java
@@ -1,5 +1,10 @@
-package com.gl.springboot.EmployeeManagement.repo;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gl.springboot.EmployeeManagement.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
